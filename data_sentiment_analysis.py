5fbf44958dc69bb34fd4f90a9049234d40c3041c
sfekih <selim.fekih@epfl.ch>
2022-04-05 12:35:37 +0200

    fix import in script

--- data_sentiment_analysis.py
+++ data_sentiment_analysis.py
@@ -1,5 +1,4 @@
 from transformers import AutoModelForSequenceClassification
-from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer
 
 import numpy as np
